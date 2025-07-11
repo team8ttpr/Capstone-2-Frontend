0f0b8142eb94cb57c6c6c9bde856619aa270c03a
Finn Terdal <finnterdal@gmail.com>
2025-07-11 02:17:31 -0400

    add 404 page

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <img
+        className="spongebob-404"
+        src="/spongebob-404.webp"
+        alt="Spongebob 404"
+      />
+      <br />
+      <h1>404 - Page Not Found</h1>
+    </>
+  );
+};
+
+export default Home;
