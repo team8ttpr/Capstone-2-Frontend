eb40359b8aac7032a1e9e747ce76a82459392f43
Webbney Vallon <webbvallon1999@gmail.com>
2025-08-01 13:45:18 -0400

    feat: created additions to the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./NavBarStyles.css";
 
 const NavBar = ({ user, onLogout }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
+
+
+  
+  
+
+
   return (
     <nav className="navbar">
-      <div className="nav-brand">
-        <Link to="/">Capstone I</Link>
+      
+       <div className="dropdown">
+      <button 
+        className="dropdown-button"
+        onMouseEnter={() => setIsOpen(true)}
+        onMouseLeave={() => setIsOpen(false)}
+      >
+        <Link to="/dashboard">Dashboard</Link>
+      </button>
+      
+      {isOpen && (
+        <div 
+          className="dropdown-content"
+          onMouseEnter={() => setIsOpen(true)}
+          onMouseLeave={() => setIsOpen(false)}
+        >
+          <Link to="/dashboard/analytics">Analytics</Link>
+          <Link to="/dashboard/myplaylist">My Playlists</Link>
+          <Link to="/dashboard/topartist">Top Artists</Link>
+          <Link to="/dashboard/toptracks">Top Tracks</Link>
+        </div>
+      )}
+    </div>
+     
+     <div className="dropdown">
+      <button 
+        className="dropdown-button"
+        onMouseEnter={() => setIsOpen2(true)}
+        onMouseLeave={() => setIsOpen2(false)}
+      >
+        <Link to="/social">Social</Link>
+      </button>
+      
+      {isOpen2 && (
+        <div 
+          className="dropdown-content"
+          onMouseEnter={() => setIsOpen2(true)}
+          onMouseLeave={() => setIsOpen2(false)}
+        >
+          <Link to="/social/feed">Feed</Link>
+          <Link to="/social/mypost">My Posts</Link>
+          <Link to="/social/friends">Friends</Link>
+          <Link to="/social/messages">Messages</Link>
+          <Link to="/social/notifications">Notifications</Link>
+        </div>
+      )}
+    </div>
+     
+      <div className="ai-playlist">
+        <Link to="/">AI Playlist</Link>
+      </div>
+
+      <div className="my-profile">
+        <Link to="/">My Profile</Link>
       </div>
 
       <div className="nav-links">
