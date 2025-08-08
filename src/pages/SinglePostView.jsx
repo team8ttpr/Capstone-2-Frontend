4da493dcfc1cf81db790ce6fdf9080613d0d4442
Webbney Vallon <webbvallon1999@gmail.com>
2025-08-08 15:00:55 -0400

    single post view in progress

---
+++ src/pages/SinglePostView.jsx
@@ -0,0 +1,22 @@
+import React, {} from "react";
+import axios from "axios";
+import {API_URL} from  "../shared";
+import { useNavigate } from "react-router-dom";
+
+const singlePostView = () => {
+
+
+
+
+
+
+    return (
+     <div>
+        <h1>welcome to the single post page</h1>
+        <p>this page well display </p>
+     </div>
+
+    );
+};
+
+export default singlePostView;
