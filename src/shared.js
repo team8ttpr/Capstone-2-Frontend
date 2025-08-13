b7b1455f1efd788e4511b0249e8754e8983119f8
Franccesco Petta <franccescopettamuller@gmail.com>
2025-08-13 18:46:53 -0400

    env change

--- src/shared.js
+++ src/shared.js
@@ -1,1 +1,2 @@
-export const API_URL = process.env.API_URL || "http://localhost:8080";
+export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
+export const FRONTEND_URL = process.env.REACT_APP_FRONTEND_URL || "http://localhost:3000";
