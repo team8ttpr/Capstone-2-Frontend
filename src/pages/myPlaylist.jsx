53c329e52b6559d0c3bf55d6d57d4cb2f218771f
Webbney Vallon <webbvallon1999@gmail.com>
2025-08-19 20:34:13 -0400

    feat: completed AI chatbox

--- src/pages/myPlaylist.jsx
+++ src/pages/myPlaylist.jsx
@@ -135,7 +135,6 @@ const MyPlaylist = ({ user }) => {
                               By: {playlist.owner.display_name}
                             </p>
                             {playlist.external_urls?.spotify && (
-                              
                               <a
                                 href={playlist.external_urls.spotify}
                                 target="_blank"
@@ -167,3 +166,4 @@ const MyPlaylist = ({ user }) => {
 };
 
 export default MyPlaylist;
+ 
